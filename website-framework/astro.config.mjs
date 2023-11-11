8ac28ba4913d2de22dfcca5bce55df638af77132
CYPHER0041 <jayanthsreejith13@gmail.com>
2023-11-12 01:19:47 +0530

    edited deployment params

--- website-framework/astro.config.mjs
+++ website-framework/astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:'https://CYPHER0041.github.io',
+    base:'/astro-test',
+});
